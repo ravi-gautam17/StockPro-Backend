e32ec1dffc8002155e0f3920f57f0c1ed77c9122
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-04-03 11:47:47 +0530

    [Ravi]: added the initial project structure for auth

---
+++ stockpro-backend/src/main/java/com/stockpro/stockpro_backend/StockproBackendApplication.java
@@ -0,0 +1,14 @@
+package com.stockpro.stockpro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockproBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StockproBackendApplication.class, args);
+	}
+
+}
---
+++ stockpro-backend/src/test/java/com/stockpro/stockpro_backend/StockproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpro.stockpro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
